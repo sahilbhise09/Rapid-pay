c9660d47595b03bb6d1c892cb6099b95e5f32448
sahilbhise09 <sahilbhise09@gmail.com>
2024-06-16 21:50:15 +0530

    Backend Setup

--- server/server.js
+++ server/server.js
@@ -5,12 +5,7 @@ const connectDB = require("./config/dbConfig.js");
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
-  await connectDB();
-
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-  });
-};
-
-startServer();
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+  connectDB();
+});
