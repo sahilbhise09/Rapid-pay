da8017ea8908b11a9d9c4058c8c57ae96fc5e35e
sahilbhise09 <sahilbhise09@gmail.com>
2024-06-20 16:56:54 +0530

    updated login

--- client/src/apicalls/users.js
+++ client/src/apicalls/users.js
@@ -5,7 +5,10 @@ const { axiosInstance } = require(".");
 
 export const LoginUser = async (payload) => {
   try {
-    const { data } = await axiosInstance.post("/api/users/login", payload);
+    const { data } = await axiosInstance.post(
+      "https://rapid-pay-szad.onrender.com/api/users/login",
+      payload
+    );
     return data;
   } catch (error) {
     return error.response.data;
